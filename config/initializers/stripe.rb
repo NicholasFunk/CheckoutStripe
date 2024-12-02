80eef3c61f1c41a1e24841baddf389f69849b6ce
nfunk <nf4313@hotmail.com>
2024-12-02 12:38:52 -0600

    Added Stripe gem, secret key and public key to the initializer.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
-PUBLISHABLE_KEY = 'pk_test_51QP5VAIi3usxqjrNtff8IAXrjIjSsDiwQ78ZvasJ6g9PRMDVlNhzI9nkBroBvTcAvMF2JlzQnIbOOHdJpWZbWNxK00UiIsO2V4'
-SECRET_KEY = 'sk_test_51QP5VAIi3usxqjrNQniBX9WRuC1jHC1TMhKXfqDq94Q3ust1pAVirMz4st9eDKcs5zMkGeaYV9TDyHSDBBGhfy8F007olF02ra'
+require "stripe"
+
+PUBLISHABLE_KEY = Rails.application.credentials[:stripe][:public]
+SECRET_KEY = Rails.application.credentials[:stripe][:secret]
 
 Rails.configuration.stripe = {
     publishable_key: PUBLISHABLE_KEY,
